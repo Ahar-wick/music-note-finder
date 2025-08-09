91174046c59a50dfbcf332697a72caf49a5da567
Aharnish Ukey <aharnish.raisoni@gmail.com>
2025-08-09 18:40:02 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .routes import main
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB upload limit
     app.config['UPLOAD_FOLDER'] = 'uploads'
     app.register_blueprint(main)
